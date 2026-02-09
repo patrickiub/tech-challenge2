9a6b6072163c36c78d28ba8bb0eea1c5f2873922
Patrick N A <patrickdevtech@gmail.com>
2026-02-09 20:28:33 -0300

    :tada: Estrutura inicial de Clean Arch

---
+++ src/test/java/br/com/fiap/postech/techchallenge2/TechChallenge2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.techchallenge2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallenge2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
