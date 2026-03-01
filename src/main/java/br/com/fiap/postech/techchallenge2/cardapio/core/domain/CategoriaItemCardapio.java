3dffc1175b6bf3f78548b592e134300ba82295f0
Patrick N A <patrickdevtech@gmail.com>
2026-03-01 11:15:08 -0300

    :sparkles: feat(cardapio): adicionar domínio ItemCardapio, enum CategoriaItemCardapio e exceção

---
+++ src/main/java/br/com/fiap/postech/techchallenge2/cardapio/core/domain/CategoriaItemCardapio.java
@@ -0,0 +1,9 @@
+package br.com.fiap.postech.techchallenge2.cardapio.core.domain;
+
+public enum CategoriaItemCardapio {
+    ENTRADA,
+    PRATO_PRINCIPAL,
+    SOBREMESA,
+    BEBIDA,
+    LANCHE
+}
